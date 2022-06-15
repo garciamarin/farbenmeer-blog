c82417bb0014edb5030efe69d943a7a2ec8e97d0
garciamarin <gsosa_97@hotmail.com>
2022-06-15 02:17:27 +0200

    feat: add 404 page

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+export default function NotFound() {
+  return (
+    <>
+        <div>Sorry, you! No page here.</div>
+        <h2> 4 0 4 </h2>
+        <p>
+            I can take you <Link href="/gql">back</Link>.
+        </p> 
+    </>
+  )
+}
